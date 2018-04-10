3b7de680dc8725093ab755c4bb36c04359eb19a4
ajo thomas <ajo.thomas24@gmail.com>
2018-04-10 07:09:18 -0700

    Merge sort changed

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -1,34 +1,31 @@
 package src.Sorting;
 
+import java.util.Arrays;
+
 /**
  * Created by ajothomas on 3/21/17.
  */
 public class MergeSort {
-  public static void printArr(int arr[]) {
-    for (int element : arr) {
-      System.out.print(element + ",");
-    }
-  }
-
   public static void merge(int arr[], int left, int middle, int right) {
-    int length = right - left + 1;
-    int[] tempArr = new int[length];
-    int i = left, j = middle + 1, k = 0;
+    int[] tempArr = new int[right - left + 1];
+    int i = left;
+    int j = middle + 1;
+    int k = 0;
 
     while (i <= middle && j <= right) {
-      if (arr[i] < arr[j])
+      if (arr[i] < arr[j]) {
         tempArr[k++] = arr[i++];
-      else
+      } else {
         tempArr[k++] = arr[j++];
+      }
     }
 
-    while (i <= middle)
-      tempArr[k++] = arr[i++];
-    while (j <= right)
-      tempArr[k++] = arr[j++];
+    while (i <= middle) tempArr[k++] = arr[i++];
+    while (j <= right) tempArr[k++] = arr[j++];
 
-    for (int element : tempArr)
+    for (int element: tempArr) {
       arr[left++] = element;
+    }
   }
 
   public static void mergeSortUtil(int[] arr, int left, int right) {
@@ -41,11 +38,11 @@ public class MergeSort {
   }
 
   public static void main(String args[]) {
-    int arr[] = {11, 3, 4, 5, 1, 9, 10, 12};
-    System.out.println("\nInitial Array");
-    printArr(arr);
+    int[] arr = {5, 3, 4, 1, 2, 7, 8, 6, 9, 0};
+    System.out.println("Before Sort");
+    System.out.println(Arrays.toString(arr));
     mergeSortUtil(arr, 0, arr.length - 1);
-    System.out.println("\nSorted Array");
-    printArr(arr);
+    System.out.println("After Sort");
+    System.out.println(Arrays.toString(arr));
   }
 }
